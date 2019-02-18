5e2424ff5d8d81a69e6f0750f19b4c5e5c6b5920
lemon4ex <netchen@vip.qq.com>
2019-02-19 03:58:12 +0800

    Initial Commit

---
+++ Reveal2Loader/Reveal2Loader.h
@@ -0,0 +1,9 @@
+//
+//  Reveal2Loader.h
+//  Reveal2Loader
+//
+//  Created by h4ck on 2019/2/19.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Reveal2Loader/Reveal2Loader.m
@@ -0,0 +1,15 @@
+//
+//  Reveal2Loader.m
+//  Reveal2Loader
+//
+//  Created by h4ck on 2019/2/19.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Reveal2Loader.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ Reveal2Loader loaded ++++++++");
+}
